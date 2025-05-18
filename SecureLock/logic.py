cf4b3bceba0e465d436717235332de76fe31aa88
Ankon <niamulankon966@gmail.com>
2025-05-18 22:01:57 +0600

    New Commit

--- SecureLock/logic.py
+++ SecureLock/logic.py
@@ -1,6 +1,7 @@
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QFileDialog, QInputDialog, QMessageBox
 from PyQt5.QtCore import Qt
+from PyQt5.QtCore import QObject, QThread, pyqtSignal
 from main_window import *
 import os
 import json
@@ -8,6 +9,28 @@ from themes import *
 from locker import *
 
 #--------------------------------------------------------------------------------------------------#
+# Worker class for folder locking
+class FolderWorker(QObject):
+    finished = pyqtSignal()
+    error = pyqtSignal(str)
+
+    def __init__(self, path, password, mode):
+        super().__init__()
+        self.path = path
+        self.password = password
+        self.mode = mode  # 'lock' or 'unlock'
+
+    def run(self):
+        try:
+            if self.mode == 'lock':
+                lock_folder(self.path, self.password)
+            elif self.mode == 'unlock':
+                unlock_folder(self.path, self.password)
+            self.finished.emit()
+        except Exception as e:
+            self.error.emit(str(e))
+#--------------------------------------------------------------------------------------------------#
+# Main application class
 class MainApp(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
@@ -41,11 +64,8 @@ class MainApp(QtWidgets.QMainWindow):
         if folder:
             pwd, ok = QInputDialog.getText(self, "Password", "Set password:", QtWidgets.QLineEdit.Password)
             if ok and pwd:
-                try:
-                    lock_folder(folder, pwd)
-                    QMessageBox.information(self, "Success", "Folder locked.")
-                except Exception as e:
-                    QMessageBox.critical(self, "Error", str(e))
+                self.run_folder_worker(folder, pwd, mode="lock")
+
 
     def unlock_file(self):
         path, _ = QFileDialog.getOpenFileName(self, "Select Locked File", filter="*.locked")
@@ -65,13 +85,22 @@ class MainApp(QtWidgets.QMainWindow):
         if folder:
             pwd, ok = QInputDialog.getText(self, "Password", "Enter password:", QtWidgets.QLineEdit.Password)
             if ok and pwd:
-                try:
-                    unlock_folder(folder, pwd)
-                    QMessageBox.information(self, "Success", "Folder unlocked.")
-                except ValueError:
-                    QMessageBox.critical(self, "Error", "Incorrect password or corrupted folder.")
-                except Exception as e:
-                    QMessageBox.critical(self, "Error", str(e))
+                self.run_folder_worker(folder, pwd, mode="unlock")
+    def run_folder_worker(self, path, password, mode):
+        self.thread = QThread()
+        self.worker = FolderWorker(path, password, mode)
+        self.worker.moveToThread(self.thread)
+
+        self.thread.started.connect(self.worker.run)
+        self.worker.finished.connect(self.thread.quit)
+        self.worker.finished.connect(self.worker.deleteLater)
+        self.thread.finished.connect(self.thread.deleteLater)
+
+        self.worker.error.connect(lambda e: QMessageBox.critical(self, "Error", e))
+        self.worker.finished.connect(lambda: QMessageBox.information(self, "Success", f"Folder {mode}ed successfully."))
+
+        self.thread.start()
+
 #--------------------------------------------------------------------------------------------------#
 if __name__ == "__main__":
     import sys
